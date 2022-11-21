602cbce74a4b58d02fa55533034213f9d482b86e
frongrider2 <frongrider2@gmail.com>
2022-11-22 05:20:22 +0700

    fix

--- index.js
+++ index.js
@@ -6,25 +6,24 @@ function randomValueFromArray(array) {
   return array[randomNo];
 }
 
-// setInterval(() => {
-//   const randomChoice = randomValueFromArray(images);
-//   imgElem.src = `images/${randomChoice}.jpg`;
-// }, 2000);
+setInterval(() => {
+  const randomChoice = randomValueFromArray(images);
+  imgElem.src = `images/${randomChoice}.jpg`;
+}, 2000);
 
 // Register service worker to control making site work offline
 
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker.register('/sw.js').then(() => {
-//     console.log('Service Worker Registered');
-//   });
-// }
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(() => {
+    console.log('Service Worker Registered');
+  });
+}
 
 // Code to handle install prompt on desktop
 
 let deferredPrompt;
 const addBtn = document.querySelector('.add-button');
-console.log(addBtn);
-// addBtn.style.display = 'none';
+addBtn.style.display = 'none';
 
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
@@ -33,10 +32,11 @@ window.addEventListener('beforeinstallprompt', (e) => {
   deferredPrompt = e;
   // Update UI to notify the user they can add to home screen
   addBtn.style.display = 'block';
+  console.log(e);
 
-  addBtn.addEventListener('click', () => {
+  const addTohome = () => {
     // hide our user interface that shows our A2HS button
-    // addBtn.style.display = 'none';
+    addBtn.style.display = 'none';
     // Show the prompt
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
@@ -48,5 +48,7 @@ window.addEventListener('beforeinstallprompt', (e) => {
       }
       deferredPrompt = null;
     });
-  });
+  };
+
+  addBtn.addEventListener('click', addTohome);
 });
